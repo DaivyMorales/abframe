e2fa9766e1b459cc30cc6878067b5c037ae7ac96
DaivyMorales <daivymorales7705@gmail.com>
2024-02-28 17:52:37 -0500

    feat(api): create new email from waitlist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,28 @@ import React, { useEffect, useRef } from "react";
 import { HiMail } from "react-icons/hi";
 import Image from "next/image";
 import Link from "next/link";
+import { useFormik } from "formik";
+import { api } from "@/utils/api";
 
 function Home() {
   const divRef = useRef<HTMLDivElement>(null);
 
+  const mutation = api.waitlist.create.useMutation();
+
   useEffect(() => {
     divRef.current!.focus();
   }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: async (values) => {
+      const response = await mutation.mutateAsync({
+        email: values.email,
+      });
+    },
+  });
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center  gap-8">
@@ -23,22 +38,27 @@ function Home() {
         </h3>
       </div>
       <div className="flex flex-col items-center justify-start gap-16">
-        <div className="flex flex-col gap-2">
+        <form onSubmit={formik.handleSubmit} className="flex flex-col gap-2">
           <div
             ref={divRef}
             className="focus-div flex w-[250px] items-center justify-center gap-2 rounded-md border-[1px] border-slate-600 px-3 py-3"
           >
             <HiMail size={15} />
             <input
+              name="email"
+              onChange={formik.handleChange}
               type="email"
               placeholder="youremail@abframe.com"
               className="placeholder:text-slate-500"
             />
           </div>
-          <button className="flex w-full items-center justify-center gap-3 rounded-md bg-[#FFBE18] px-7 py-3 text-[15px] font-semibold text-[#141616] text-white shadow-sm">
+          <button
+            type="submit"
+            className="flex w-full items-center justify-center gap-3 rounded-md bg-[#FFBE18] px-7 py-3 text-[15px] font-semibold text-[#141616] text-white shadow-sm"
+          >
             Join waitlist
           </button>
-        </div>
+        </form>
         {/* ME */}
         <section className="flex gap-3">
           <Link href={"https://twitter.com/_joaooo0_"} target="_blank">
@@ -47,7 +67,7 @@ function Home() {
               alt="Joao's image"
               width={45}
               height={35}
-              className="cursor-pointer rounded-full"
+              className="cursor-pointer rounded-full hover:brightness-[80%]"
               priority={true}
             />
           </Link>
@@ -56,7 +76,7 @@ function Home() {
             <Link
               href={"https://twitter.com/_joaooo0_"}
               target="_blank"
-              className="cursor-pointer text-[13px] text-slate-400"
+              className="cursor-pointer text-[13px] text-slate-400 hover:text-slate-500"
             >
               @_joaooo0_
             </Link>
